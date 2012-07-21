67ac3dcbba69f956fdacbca04b7d702da36e874e
Your Name <Rayylim@gmail.com>
2012-07-21 15:55:12 -0700

    Splash screen. Title, join button, enter player name

--- LiarPoker/gen/com/example/liarpoker/R.java
+++ LiarPoker/gen/com/example/liarpoker/R.java
@@ -15,7 +15,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int edit_message=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,8 +26,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_joingame=0x7f040006;
         public static final int button_send=0x7f040004;
+        public static final int enteryourname_message=0x7f040007;
         public static final int hello_world=0x7f040001;
+        public static final int liarpokertitle=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
     }
