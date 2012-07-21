292205f9d2bb222a53f35500f348c94bff042169
Your Name <Rayylim@gmail.com>
2012-07-21 15:41:03 -0700

    Initial Checkin

---
+++ LiarPoker/gen/com/example/liarpoker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.liarpoker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LiarPoker/src/com/example/liarpoker/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.liarpoker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
