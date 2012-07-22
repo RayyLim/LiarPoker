849fb99acb1a7d8b1e249f13769a7aab998beb69
Your Name <Rayylim@gmail.com>
2012-07-21 17:43:27 -0700

    Added Waitroom Layout

--- LiarPoker/gen/com/example/liarpoker/R.java
+++ LiarPoker/gen/com/example/liarpoker/R.java
@@ -15,11 +15,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int addplayer=0x7f070003;
         public static final int edit_message=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070004;
+        public static final int playerlist=0x7f070002;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int waitroom_main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
