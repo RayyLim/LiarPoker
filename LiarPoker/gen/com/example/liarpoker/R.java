933b8b1fe21700980ebc9932f7e422fc5b5c9aa2
Your Name <Rayylim@gmail.com>
2012-07-22 05:36:54 -0700

    Add feature where if everybody else presses not a liar button.. quickly goes to next player

--- LiarPoker/gen/com/example/liarpoker/R.java
+++ LiarPoker/gen/com/example/liarpoker/R.java
@@ -18,24 +18,22 @@ public final class R {
         public static final int currentPlayerHasLabel=0x7f070005;
         public static final int currentPlayerLabel=0x7f070004;
         public static final int edit_message=0x7f070000;
-        public static final int fakeNoOneCallsLiar=0x7f07000f;
-        public static final int fakeSomeoneCallsLiar=0x7f070010;
+        public static final int fakeNoOneCallsLiar=0x7f07000d;
+        public static final int fakeSomeoneCallsLiar=0x7f07000e;
         public static final int igotamount=0x7f070009;
         public static final int igotnumber=0x7f07000a;
         public static final int liarButton=0x7f070006;
         public static final int meLabel=0x7f070001;
-        public static final int menu_settings=0x7f070014;
+        public static final int menu_settings=0x7f070012;
         public static final int myTurnLayout=0x7f070008;
         public static final int mynumbersLabel=0x7f070002;
         public static final int notLiarButton=0x7f070007;
         public static final int notMyTurnLayout=0x7f070003;
-        public static final int playerlist=0x7f070012;
-        public static final int startgamebutton=0x7f070013;
+        public static final int playerlist=0x7f070010;
+        public static final int startgamebutton=0x7f070011;
         public static final int submitButton=0x7f07000b;
-        public static final int testMyTurn=0x7f07000c;
-        public static final int testNextPlayerTurn=0x7f07000d;
-        public static final int testRandomEntryForPlayer=0x7f07000e;
-        public static final int textView1=0x7f070011;
+        public static final int testRandomEntryForPlayer=0x7f07000c;
+        public static final int textView1=0x7f07000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
